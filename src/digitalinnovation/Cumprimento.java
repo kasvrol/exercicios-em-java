8d67a525ca31de4a65bb2c3d0462dcd00a8bed7b
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-04 14:50:56 -0300

    feat: função cumprimento

---
+++ src/digitalinnovation/Cumprimento.java
@@ -0,0 +1,12 @@
+public class Cumprimento {
+    public static void cumprimento(double hora){
+	if(4:00 =< hora =< 12:00){
+        System.out.println( "Bom dia");
+    }   else if(12:00 < hora =< 18:00){
+        System.out.println( "Boa tarde");
+    }  else    {
+        System.out.println( "Boa noite");
+    }}
+}
+
+//bootcamp "Québec Java Digital", curso "Entendendo Métodos Java", exercício "Cumprimento"
