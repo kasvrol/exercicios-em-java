d53953dcf0cdc98b5603ee8109672ad59484d56f
ana.karolina <ana.karolina@ablsystem.com.br>
2022-10-04 14:29:01 -0300

    feat: função Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+public class Calculadora {
+    public static void calculadora(double parameter1, double parameter2, String operation){
+	if(operation == "soma"){
+        double resultado = parameter1 + parameter2
+        System.out.println( "A soma dos números é" + resultado);
+    }   else if(operation == "subtração" && parameter2!=0){
+    double resultado = parameter1 - parameter2
+    System.out.println( "A subtração dos números é" + resultado);
+    }   else if(operation == "divisão"){
+    double resultado = parameter1 * parameter2
+    System.out.println( "A divisão dos números é" + resultado);
+    }   else    {
+    System.out.println( "Essa operação não existe");
+    }}
+}
